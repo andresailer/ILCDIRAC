646c0144c6d922732c4871a99f907bcf04a38d3b
Andre Sailer <andre.philippe.sailer@cern.ch>
2016-06-26 07:55:07 +0200

    py.test fixes

--- .gitlab-ci.d/run-unittests.sh
+++ .gitlab-ci.d/run-unittests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source .gitlab-ci.d/set-reportstyle.sh
-export PYTEST_ADDOPTS=$PYTEST_ADDOPTS' --ignore Interfaces/API/NewInterface/Tests/Test_FullCVMFSTests.py --ignore Workflow/Modules/Test/Test_SEs.py -k "not ILCDIRAC"'
+export PYTEST_ADDOPTS=$PYTEST_ADDOPTS" --ignore Interfaces/API/NewInterface/Tests/Test_FullCVMFSTests.py --ignore Workflow/Modules/Test/Test_SEs.py"
 py.test
